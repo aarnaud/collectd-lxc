e54da4b08307232c50240de781ba585067843482
Jonathan Ballet <jballet@edgelab.ch>
2015-12-31 10:51:22 +0100

    Simplify calls to Collectd Values() object

--- collectd_lxc.py
+++ collectd_lxc.py
@@ -50,29 +50,12 @@ def reader(input_data=None):
                         elif data[0] == "total_swap":
                                 mem_swap = int(data[1])
 
-                    memory_rss = collectd.Values()
-                    memory_rss.plugin = "lxc_memory"
-                    memory_rss.plugin_instance = lxc_fullname
-                    memory_rss.type_instance = "rss"
-                    memory_rss.type = 'gauge'
-                    memory_rss.values = [mem_rss]
-                    memory_rss.dispatch()
+                    values = collectd.Values(plugin_instance=lxc_fullname,
+                                             type="gauge", plugin="lxc_memory")
+                    values.dispatch(type_instance="rss", values=[mem_rss])
+                    values.dispatch(type_instance="cache", values=[mem_cache])
+                    values.dispatch(type_instance="swap", values=[mem_swap])
 
-                    memory_cache = collectd.Values()
-                    memory_cache.plugin = "lxc_memory"
-                    memory_cache.plugin_instance = lxc_fullname
-                    memory_cache.type_instance = "cache"
-                    memory_cache.type = 'gauge'
-                    memory_cache.values = [mem_cache]
-                    memory_cache.dispatch()
-
-                    memory_swap = collectd.Values()
-                    memory_swap.plugin = "lxc_memory"
-                    memory_swap.plugin_instance = lxc_fullname
-                    memory_swap.type_instance = "swap"
-                    memory_swap.type = 'gauge'
-                    memory_swap.values = [mem_swap]
-                    memory_swap.dispatch()
                 ### End Memory
 
                 ### CPU
@@ -90,47 +73,21 @@ def reader(input_data=None):
                         elif data[0] == "system":
                                 cpu_system = int(data[1])
 
-                    CPU_user = collectd.Values()
-                    CPU_user.plugin = "lxc_cpu"
-                    CPU_user.plugin_instance = lxc_fullname
-                    CPU_user.type_instance = "user"
-                    CPU_user.type = 'gauge'
-                    CPU_user.values = [cpu_user]
-                    CPU_user.dispatch()
-
-                    CPU_system = collectd.Values()
-                    CPU_system.plugin = "lxc_cpu"
-                    CPU_system.plugin_instance = lxc_fullname
-                    CPU_system.type_instance = "system"
-                    CPU_system.type = 'gauge'
-                    CPU_system.values = [cpu_system]
-                    CPU_system.dispatch()
+                    values = collectd.Values(plugin_instance=lxc_fullname,
+                                             type="gauge", plugin="lxc_cpu")
+                    values.dispatch(type_instance="user", values=[cpu_user])
+                    values.dispatch(type_instance="system", values=[cpu_system])
 
                 ### End CPU
 
                 ### DISK
                 if metric == "blkio":
+
                     with open(os.path.join(metrics[user_id][container_name][metric], 'blkio.throttle.io_service_bytes'), 'r') as f:
                         lines = f.read()
 
                     bytes_read = int(re.search("Read\s+(?P<read>[0-9]+)", lines).group("read"))
                     bytes_write = int(re.search("Write\s+(?P<write>[0-9]+)", lines).group("write"))
-
-                    blkio_bytes_read = collectd.Values()
-                    blkio_bytes_read.plugin = "lxc_blkio"
-                    blkio_bytes_read.plugin_instance = lxc_fullname
-                    blkio_bytes_read.type_instance = "bytes_read"
-                    blkio_bytes_read.type = 'gauge'
-                    blkio_bytes_read.values = [bytes_read]
-                    blkio_bytes_read.dispatch()
-
-                    blkio_bytes_write = collectd.Values()
-                    blkio_bytes_write.plugin = "lxc_blkio"
-                    blkio_bytes_write.plugin_instance = lxc_fullname
-                    blkio_bytes_write.type_instance = "bytes_write"
-                    blkio_bytes_write.type = 'gauge'
-                    blkio_bytes_write.values = [bytes_write]
-                    blkio_bytes_write.dispatch()
 
                     with open(os.path.join(metrics[user_id][container_name][metric], 'blkio.throttle.io_serviced'), 'r') as f:
                         lines = f.read()
@@ -138,21 +95,12 @@ def reader(input_data=None):
                     ops_read = int(re.search("Read\s+(?P<read>[0-9]+)", lines).group("read"))
                     ops_write = int(re.search("Write\s+(?P<write>[0-9]+)", lines).group("write"))
 
-                    blkio_io_read = collectd.Values()
-                    blkio_io_read.plugin = "lxc_blkio"
-                    blkio_io_read.plugin_instance = lxc_fullname
-                    blkio_io_read.type_instance = "ops_read"
-                    blkio_io_read.type = 'gauge'
-                    blkio_io_read.values = [ops_read]
-                    blkio_io_read.dispatch()
-
-                    blkio_io_write = collectd.Values()
-                    blkio_io_write.plugin = "lxc_blkio"
-                    blkio_io_write.plugin_instance = lxc_fullname
-                    blkio_io_write.type_instance = "ops_write"
-                    blkio_io_write.type = 'gauge'
-                    blkio_io_write.values = [ops_write]
-                    blkio_io_write.dispatch()
+                    values = collectd.Values(plugin_instance=lxc_fullname,
+                                             type="gauge", plugin="lxc_blkio")
+                    values.dispatch(type_instance="bytes_read", values=[bytes_read])
+                    values.dispatch(type_instance="bytes_write", values=[bytes_write])
+                    values.dispatch(type_instance="ops_read", values=[ops_read])
+                    values.dispatch(type_instance="ops_write", values=[ops_write])
 
                 ### End DISK
 
@@ -160,12 +108,14 @@ def reader(input_data=None):
 if __name__ == '__main__':
     # Mimic Collectd Values object
     class Values(object):
-        def __init__(self):
-            self.__dict__["_values"] = {}
+        def __init__(self, **kwargs):
+            self.__dict__["_values"] = kwargs
         def __setattr__(self, key, value):
             self.__dict__["_values"][key] = value
-        def dispatch(self):
-            print(self._values)
+        def dispatch(self, **kwargs):
+            values = self._values.copy()
+            values.update(kwargs)
+            print(values)
 
     import types
     collectd = types.ModuleType("collectd")
